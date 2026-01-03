0c265e5be7b5f1fd96c8f660b469a3411497fb15
ohmusic123-del <ohmusic.123@gmail.com>
2026-01-03 21:57:14 +0530

    Delete js/js directory

--- js/js/game.js
+++
@@ -1,9 +0,0 @@
-let time=30,roundId=Date.now()+"";
-setInterval(()=>{
-  timer.innerText="00:"+String(time--).padStart(2,"0");
-  if(time<0){time=30;apiPost("/round/resolve",{roundId});roundId=Date.now()+"";}
-},1000);
-
-function bet(color){
-  apiPost("/bet",{color,amount:1,roundId},localStorage.token);
-}
